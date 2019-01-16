350d84b1aafc9e6694c097fd49db8ada517254f5
Jonny Wildey <jonny@trussle.com>
2019-01-16 19:26:01 +0000

    test sort of works

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
-  app.on(['pull_request.opened', 'pull_request.synchonize'], async (context) => {
+  app.log("Waiting for CHONK");
+  app.on(['pull_request.opened', 'pull_request.synchronize'], async (context) => {
     app.log(context);
     await context.github.issues.createLabel({
       color: "008330",
       description: "CHONK",
       name: "A Fine Boi",
       owner: "Chonk",
-      repo: context.payload.pull_request.repo.id
+      repo: context.payload.pull_request.head.repo.id
     })
   })
 }
