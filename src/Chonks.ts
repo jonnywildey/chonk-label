bb3e9fa4835d41cfc023f4d7fae6ccab0dac718c
Jonny Wildey <jonny@trussle.com>
2019-01-29 17:57:40 +0000

    refactor

---
+++ src/Chonks.ts
@@ -0,0 +1,42 @@
+export interface IChonk {
+  name: string;
+  color: string;
+  fileLimit: number;
+}
+
+export const Chonks: IChonk[] = [
+  {
+    name: "A Fine Boi",
+    color: "01c528",
+    fileLimit: 10
+  },
+  {
+    name: "They Chomnk",
+    color: "ddb706",
+    fileLimit: 20
+  },
+  {
+    name: "A Hecklin' Chonker",
+    color: "fea000",
+    fileLimit: 40
+  },
+  {
+    name: "HEFTYCHONK",
+    color: "ff7300",
+    fileLimit: 80
+  },
+  {
+    name: "MEGACHONKER",
+    color: "fd0000",
+    fileLimit: 160
+  },
+  {
+    name: "OH LAWD HE COMIN",
+    color: "c70304",
+    fileLimit: Infinity
+  }
+];
+
+export const getChonk = (filesChanged: number): IChonk => {
+  return Chonks.find(c => filesChanged < c.fileLimit)!;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,51 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { Application, Context } from 'probot' // eslint-disable-line no-unused-vars
+import { Chonks, getChonk, IChonk } from "./Chonks";
 
 export = (app: Application) => {
   app.log("Waiting for CHONK");
   app.on(['pull_request.opened', 'pull_request.synchronize'], async (context) => {
-    // app.log(context);
-    // const label = context.issue({
-    //   color: "008330",
-    //   description: "CHONK",
-    //   name: "bug"
-    // });
-    // const commits = await context.github.pullRequests.(context.repo({ number: pr.number }))
 
     const filesChanged = context.payload.pull_request.changed_files;
-    const chonk = getChonk(filesChanged);
+    const selectedChonk = getChonk(filesChanged);
 
-
-    const message = `This PR has been labeled as **${chonk.text}** ![#${chonk.color}](https://placehold.it/15/${chonk.color}/000000?text=+)`
-    context.github.issues.createComment(context.issue({
-      body: message
-    }));
-
-    const labels = await context.github.issues.listLabelsForRepo(context.issue({}));
-    console.log("labels", labels);
-    const createPromises = await Promise.all(chonks.map(async c => {
-      if (labels.data.find(d => d.name === c.text)) {
-        return;
-      }
-      return context.github.issues.createLabel(context.issue({
-        name: c.text,
-        color: c.color,
-        description: c.text
-      }));
-    }));
-
-    await context.github.issues.addLabels(context.issue({
-      labels: [chonk.text]
-    }));
-
+    await createChonkLabels(context);
+    await updateLabel(context, selectedChonk);
   })
 }
 
-const createChonkLabels = (context: any) => {
-
-}
-
-interface IChonk {
-  text: string;
-  color: string;
-  fileLimit: number;
-}
-
-const chonks: IChonk[] = [
-  {
-    text: "A Fine Boi",
-    color: "01c528",
-    fileLimit: 10
-  },
-  {
-    text: "They Chomnk'",
-    color: "ddb706",
-    fileLimit: 20
-  },
-  {
-    text: "A Hecklin' Chonker",
-    color: "fea000",
-    fileLimit: 40
-  },
-  {
-    text: "HEFTYCHONK",
-    color: "ff7300",
-    fileLimit: 80
-  },
-  {
-    text: "MEGACHONKER",
-    color: "fd0000",
-    fileLimit: 160
-  },
-  {
-    text: "OH LAWD HE COMIN",
-    color: "c70304",
-    fileLimit: Infinity
+const updateLabel = async (context: Context, selectedChonk: IChonk) => {
+  const labels = await context.github.issues.listLabelsOnIssue(context.issue({}));
+  if (labels.data.find(l => l.name === selectedChonk.name)) {
+    context.log(`Label is correct`);
+    return;
   }
-];
+  // Remove previous chonk labels
+  await Promise.all(Chonks.map(async (c) => {
+    if (labels.data.find(l => l.name === c.name)) {
+      context.log(`Deleting label ${c.name}`);
+      return context.github.issues.deleteLabel(context.issue({
+        name: c.name
+      }))
+    }
+    return;
+  }))
+  // Add a new label
+  context.log(`Adding label ${selectedChonk.name}`);
+  await context.github.issues.addLabels(context.issue({
+    labels: [selectedChonk.name]
+  }));
+}
 
-const getChonk = (filesChanged: number): IChonk => {
-  return chonks.find(c => filesChanged < c.fileLimit)!;
+const createChonkLabels = async (context: Context) => {
+  const labels = await context.github.issues.listLabelsForRepo(context.issue({}));
+  await Promise.all(Chonks.map(async c => {
+    if (labels.data.find(d => d.name === c.name)) {
+      return;
+    }
+    return context.github.issues.createLabel(context.issue({
+      name: c.name,
+      color: c.color,
+      description: c.name
+    }));
+  }));
 }
